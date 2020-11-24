7db93e154f27c3a1acd623013279c6e62b137ced
havryliuk-andrii <havryliuk.andrii.00@gmail.com>
2020-11-24 23:55:50 +0200

    add index.html

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
+
 const server = http.createServer(app);
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
- res.status(200).send(`Hello World! Our server is running at port ${port}`);
+ res.status(200).send(path.join(__dirname + '/src/index.html'));
 });
 
 server.listen(port, () => {
