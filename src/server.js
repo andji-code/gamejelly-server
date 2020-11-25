1ed34f0ecf9161f89665f098edcec2538385dfdd
havryliuk-andrii <havryliuk.andrii.00@gmail.com>
2020-11-25 10:52:27 +0200

    ci

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   console.log("wwwwwwwwwwwwww")
- res.status(200).send(path.join(__dirname + '/src/index.html'));
+ res.status(200).sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 server.listen(port, () => {
